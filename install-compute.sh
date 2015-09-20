55c55d4cb07a904b2bae8d714dca6b4c1d16fae6
thanhtien522 <thanhtien522@gmail.com>
2015-09-20 22:03:13 +0700

    Update install compute

--- install-compute.sh
+++ install-compute.sh
@@ -3,18 +3,19 @@
 export BASE_DIR=$( cd `dirname $0` && pwd )
 
 source ${BASE_DIR}/common/openstack.conf
-
+mkdir ${BASE_DIR}/log
+touch ${BASE_DIR}/log/compute.log
 echo "Add hostname to /etc/host ..."
-${BASE_DIR}/tools/addHost.sh | tee ${BASE_DIR}/log/compute.log
+${BASE_DIR}/tools/addHost.sh | tee -a ${BASE_DIR}/log/compute.log
 echo "Done."
 echo "Installing Network Time Protocol ..."
-${BASE_DIR}/scripts/install-ntp-compute.sh | tee ${BASE_DIR}/log/compute.log
+${BASE_DIR}/scripts/env/install-ntp-compute.sh | tee -a ${BASE_DIR}/log/compute.log
 echo "Done."
 echo "Installing OpenStack packages..."
-${BASE_DIR}/scripts/install-openstack-package.sh | tee ${BASE_DIR}/log/compute.log
+${BASE_DIR}/scripts/env/install-openstack-package.sh | tee -a ${BASE_DIR}/log/compute.log
 echo "Done."
 
 
 echo "Installing Nova..."
-${BASE_DIR}/scripts/install-nova-compute.sh | tee ${BASE_DIR}/log/compute.log
+${BASE_DIR}/scripts/nova/install-nova-compute.sh | tee -a ${BASE_DIR}/log/compute.log
 echo "Done."
