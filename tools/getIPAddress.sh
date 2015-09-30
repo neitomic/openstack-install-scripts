b16d797da7b0c98d9892c732716c2ef51749ac88
Nguyễn Thanh Tiến <thanhtien522@gmail.com>
2015-09-30 10:40:53 +0700

    Fixed error for get ip address tool.

--- tools/getIPAddress.sh
+++ tools/getIPAddress.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 NETWORK_PREFIX=${NETWORK_PREFIX:-172.28.181}
-IPADDR=$(ifconfig | grep ${NETWORK_PREFIX} | awk '{print $2}')
+IPADDR=$(ifconfig | grep ${NETWORK_PREFIX} | awk '{print $2}' | awk -F: '{print $2}')
 echo ${IPADDR}
