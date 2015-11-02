be05157bb4417703407276fc3b176c046d9b295b
Nguyễn Thanh Tiến <thanhtien522@gmail.com>
2015-11-02 20:27:18 +0700

    Update script paths

--- install-compute.sh
+++ install-compute.sh
@@ -11,13 +11,13 @@ echo "Add hostname to /etc/host ..."
 ${BASE_DIR}/tools/addHost.sh | tee ${BASE_DIR}/log/compute.log
 echo "Done."
 echo "Installing Network Time Protocol ..."
-${BASE_DIR}/scripts/install-ntp-compute.sh | tee ${BASE_DIR}/log/compute.log
+${BASE_DIR}/scripts/env/install-ntp-compute.sh | tee ${BASE_DIR}/log/compute.log
 echo "Done."
 echo "Installing OpenStack packages..."
-${BASE_DIR}/scripts/install-openstack-package.sh | tee ${BASE_DIR}/log/compute.log
+${BASE_DIR}/scripts/env/install-openstack-package.sh | tee ${BASE_DIR}/log/compute.log
 echo "Done."
 
 
 echo "Installing Nova..."
-${BASE_DIR}/scripts/install-nova-compute.sh | tee ${BASE_DIR}/log/compute.log
+${BASE_DIR}/scripts/nova/install-nova-compute.sh | tee ${BASE_DIR}/log/compute.log
 echo "Done."
--- install-controller.sh
+++ install-controller.sh
@@ -11,26 +11,26 @@ echo "Add hostname to /etc/host ..."
 ${BASE_DIR}/tools/addHost.sh | tee ${BASE_DIR}/log/controller.log
 echo "Done."
 echo "Installing Network Time Protocol ..."
-${BASE_DIR}/scripts/install-ntp-controller.sh | tee ${BASE_DIR}/log/controller.log
+${BASE_DIR}/scripts/env/install-ntp-controller.sh | tee ${BASE_DIR}/log/controller.log
 echo "Done."
 echo "Installing OpenStack packages..."
-${BASE_DIR}/scripts/install-openstack-package.sh | tee ${BASE_DIR}/log/controller.log
+${BASE_DIR}/scripts/env/install-openstack-package.sh | tee ${BASE_DIR}/log/controller.log
 echo "Done."
 echo "Installing SQL database..."
-${BASE_DIR}/scripts/install-mariadb.sh | tee ${BASE_DIR}/log/controller.log
+${BASE_DIR}/scripts/env/install-mariadb.sh | tee ${BASE_DIR}/log/controller.log
 echo "Done."
 echo "Installing Message queue (RabbitMq) ..."
-${BASE_DIR}/scripts/install-rabbitmq.sh | tee ${BASE_DIR}/log/controller.log
+${BASE_DIR}/scripts/env/install-rabbitmq.sh | tee ${BASE_DIR}/log/controller.log
 echo "Done."
 
 echo "Installing KeyStone..."
-${BASE_DIR}/scripts/install-keystone-controller.sh | tee ${BASE_DIR}/log/controller.log
+${BASE_DIR}/scripts/keystone/install-keystone.sh | tee ${BASE_DIR}/log/controller.log
 echo "Done."
 
 echo "Installing Glance..."
-${BASE_DIR}/scripts/install-glance-controller.sh | tee ${BASE_DIR}/log/controller.log
+${BASE_DIR}/scripts/glance/install-glance.sh | tee ${BASE_DIR}/log/controller.log
 echo "Done."
 
 echo "Installing Nova..."
-${BASE_DIR}/scripts/install-nova-controller.sh | tee ${BASE_DIR}/log/controller.log
+${BASE_DIR}/scripts/nova/install-nova-controller.sh | tee ${BASE_DIR}/log/controller.log
 echo "Done."
