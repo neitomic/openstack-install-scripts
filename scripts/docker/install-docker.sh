e170c088508427b2f41334f3cd291c38e1f519ea
Nguyễn Thanh Tiến <thanhtien522@gmail.com>
2015-11-23 19:48:43 +0700

    Switch nova-docker to cinder test repository

--- scripts/docker/install-docker.sh
+++ scripts/docker/install-docker.sh
@@ -13,7 +13,7 @@ systemctl start docker
 
 cd ${BASE_DIR}
 
-git clone -b stable/kilo https://github.com/openstack/nova-docker.git
+git clone -b test/kilo-cinder https://github.com/thanhtien522/nova-docker.git
 cd nova-docker
 python setup.py install
 
